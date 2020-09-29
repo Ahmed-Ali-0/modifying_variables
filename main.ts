32c6a302782bf4585745a6a34d6d9a3d822fcc67
Ahmed-Ali-0 <71400128+Ahmed-Ali-0@users.noreply.github.com>
2020-09-28 22:19:31 -0400

    add comment

--- main.ts
+++ main.ts
@@ -3,5 +3,8 @@ basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         modifying_Variables = modifying_Variables + 1
         basic.showNumber(modifying_Variables)
+    } else if (input.buttonIsPressed(Button.B)) {
+        modifying_Variables = 0
+        basic.showNumber(modifying_Variables)
     }
 })
